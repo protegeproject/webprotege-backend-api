438cc591a389744994cfd9175046398786ea0f23
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-29 17:33:22 +0100

    Added missing projectId field

--- src/main/java/edu/stanford/protege/webprotege/projectsettings/ProjectSettingsChangedEvent.java
+++ src/main/java/edu/stanford/protege/webprotege/projectsettings/ProjectSettingsChangedEvent.java
@@ -3,6 +3,7 @@ package edu.stanford.protege.webprotege.projectsettings;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import edu.stanford.protege.webprotege.common.Event;
+import edu.stanford.protege.webprotege.common.ProjectId;
 
 /**
  * Matthew Horridge
@@ -10,7 +11,7 @@ import edu.stanford.protege.webprotege.common.Event;
  * 25/11/14
  */
 @JsonTypeName("ProjectSettingsChangedEvent")
-public record ProjectSettingsChangedEvent(ProjectSettings projectSettings) implements Event {
+public record ProjectSettingsChangedEvent(ProjectId projectId, ProjectSettings projectSettings) implements Event {
 
     public static final String CHANNEL = "webprotege.project.events.ProjectSettingsChanged";
 
