e1206b9dfd5a7257d41542f405c41bf672e07f42
Matthew Horridge <matthew.horridge@stanford.edu>
2021-09-01 13:11:15 +0100

    Add project id to the response

--- src/main/java/edu/stanford/protege/webprotege/axioms/AddAxiomsResponse.java
+++ src/main/java/edu/stanford/protege/webprotege/axioms/AddAxiomsResponse.java
@@ -1,5 +1,6 @@
 package edu.stanford.protege.webprotege.axioms;
 
+import edu.stanford.protege.webprotege.common.ProjectId;
 import edu.stanford.protege.webprotege.common.Response;
 
 /**
@@ -7,6 +8,7 @@ import edu.stanford.protege.webprotege.common.Response;
  * Stanford Center for Biomedical Informatics Research
  * 2021-09-01
  */
-public record AddAxiomsResponse(int addedAxiomsCount) implements Response {
+public record AddAxiomsResponse(ProjectId projectId,
+                                int addedAxiomsCount) implements Response {
 
 }
