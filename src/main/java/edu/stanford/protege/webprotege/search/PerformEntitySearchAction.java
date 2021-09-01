5e7eb9f1bd45ed4c77ab8beb66d138aa25ef4c1d
Matthew Horridge <matthew.horridge@stanford.edu>
2021-09-01 20:09:07 +0100

    Added defaults for fields in PerformEntitySearch

--- src/main/java/edu/stanford/protege/webprotege/search/PerformEntitySearchAction.java
+++ src/main/java/edu/stanford/protege/webprotege/search/PerformEntitySearchAction.java
@@ -3,6 +3,7 @@ package edu.stanford.protege.webprotege.search;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import edu.stanford.protege.webprotege.common.LangTagFilter;
 import edu.stanford.protege.webprotege.common.ProjectId;
 import edu.stanford.protege.webprotege.dispatch.ProjectAction;
@@ -11,7 +12,12 @@ import edu.stanford.protege.webprotege.project.HasProjectId;
 import org.semanticweb.owlapi.model.EntityType;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.Objects.requireNonNullElse;
 
 /**
  * Matthew Horridge
@@ -21,14 +27,28 @@ import java.util.Set;
 
 
 @JsonTypeName("PerformEntitySearch")
-public record PerformEntitySearchAction(@JsonProperty("projectId") @Nonnull ProjectId projectId,
-                                        @JsonProperty("searchString") @Nonnull String searchString,
-                                        @JsonProperty("entityTypes") @Nonnull Set<EntityType<?>> entityTypes,
+public record PerformEntitySearchAction(@JsonProperty(value = "projectId", required = true) @Nonnull ProjectId projectId,
+                                        @JsonProperty(value = "searchString", required = true) @Nonnull String searchString,
+                                        @JsonProperty(value = "entityTypes", defaultValue = "[]") @Nonnull Set<EntityType<?>> entityTypes,
                                         @JsonProperty("langTagFilter") @Nonnull LangTagFilter langTagFilter,
                                         @JsonProperty("searchFilters") @Nonnull ImmutableList<EntitySearchFilter> searchFilters,
                                         @JsonProperty("pageRequest") @Nonnull PageRequest pageRequest) implements ProjectAction<PerformEntitySearchResult>, HasProjectId {
 
     public static final String CHANNEL = "webprotege.search.PerformEntitySearch";
+
+    public PerformEntitySearchAction(@JsonProperty("projectId") @Nonnull ProjectId projectId,
+                                     @JsonProperty("searchString") @Nonnull String searchString,
+                                     @JsonProperty(value = "entityTypes") @Nonnull Set<EntityType<?>> entityTypes,
+                                     @JsonProperty("langTagFilter") @Nonnull LangTagFilter langTagFilter,
+                                     @JsonProperty("searchFilters") @Nonnull ImmutableList<EntitySearchFilter> searchFilters,
+                                     @JsonProperty("pageRequest") @Nonnull PageRequest pageRequest) {
+        this.projectId = requireNonNull(projectId);
+        this.searchString = requireNonNull(searchString);
+        this.entityTypes = requireNonNullElse(entityTypes, Collections.emptySet());
+        this.langTagFilter = requireNonNullElse(langTagFilter, LangTagFilter.get(ImmutableSet.of()));
+        this.searchFilters = requireNonNullElse(searchFilters, ImmutableList.of());
+        this.pageRequest = requireNonNullElse(pageRequest, PageRequest.requestFirstPage());
+    }
 
     @Override
     public String getChannel() {
