d3fdf867623c2f84040d2b1e9ba775f712475ae7
Matthew Horridge <matthew.horridge@stanford.edu>
2021-09-23 13:27:09 -0700

    Added test for OboIdSuffixSettings

--- src/test/java/edu/stanford/protege/webprotege/crud/obo/OboIdSuffixSettings_SerializationTestCase.java
+++ src/test/java/edu/stanford/protege/webprotege/crud/obo/OboIdSuffixSettings_SerializationTestCase.java
@@ -7,19 +7,25 @@ import edu.stanford.protege.webprotege.crud.oboid.OboIdSuffixSettings;
 import edu.stanford.protege.webprotege.crud.oboid.UserIdRange;
 
 import edu.stanford.protege.webprotege.common.UserId;
-import org.junit.Before;
-import org.junit.Test;
+import edu.stanford.protege.webprotege.jackson.WebProtegeJacksonApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 2020-04-07
  */
+@JsonTest
+@Import(WebProtegeJacksonApplication.class)
 public class OboIdSuffixSettings_SerializationTestCase {
 
     public static final int TOTAL_DIGITS = 77;
@@ -32,7 +38,10 @@ public class OboIdSuffixSettings_SerializationTestCase {
 
     private OboIdSuffixSettings settings;
 
-    @Before
+    @Autowired
+    private JacksonTester<EntityCrudKitSuffixSettings> tester;
+
+    @BeforeEach
     public void setUp() {
         settings = OboIdSuffixSettings.get(
                 TOTAL_DIGITS,
@@ -43,12 +52,19 @@ public class OboIdSuffixSettings_SerializationTestCase {
     }
 
     @Test
-    public void shouldRoundTripSettings() throws IOException {
-        
+    public void shouldSerializeSettings() throws IOException {
+        var json = tester.write(settings);
+        System.out.println(json.getJson());
+        assertThat(json).extractingJsonPathStringValue("['@type']").isEqualTo("OboId");
+        assertThat(json).extractingJsonPathNumberValue("totalDigits").isEqualTo(77);
+        assertThat(json).hasJsonPathValue("userIdRanges");
     }
 
     @Test
-    public void shouldDeserializeLongClassNameForBackwardsCompatibility() throws IOException {
-        var serialization = "{\"_class\":\"edu.stanford.bmir.protege.web.shared.crud.oboid.OBOIdSuffixSettings\",\"totalDigits\":77,\"userIdRanges\":[{\"userId\":\"TheUser\",\"start\":100,\"end\":203}]}";
+    public void shouldDeserializeSettings() throws IOException {
+        var serialization = "{\"@type\":\"OboId\",\"totalDigits\":77,\"userIdRanges\":[{\"userId\":\"TheUser\",\"start\":100,\"end\":203}]}";
+        var content = tester.parse(serialization);
+        var object = content.getObject();
+        assertThat(object).isEqualTo(settings);
     }
 }
--- src/test/java/edu/stanford/protege/webprotege/crud/persistence/ProjectEntityCrudKitSettings_SerializationTestCase.java
+++ src/test/java/edu/stanford/protege/webprotege/crud/persistence/ProjectEntityCrudKitSettings_SerializationTestCase.java
@@ -6,16 +6,25 @@ import edu.stanford.protege.webprotege.crud.gen.GeneratedAnnotationsSettings;
 import edu.stanford.protege.webprotege.crud.uuid.UuidSuffixSettings;
 
 import edu.stanford.protege.webprotege.common.ProjectId;
-import org.junit.Before;
-import org.junit.Test;
+import edu.stanford.protege.webprotege.jackson.WebProtegeJacksonApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 2020-04-07
  */
+@JsonTest
+@Import(WebProtegeJacksonApplication.class)
 public class ProjectEntityCrudKitSettings_SerializationTestCase {
 
 
@@ -23,7 +32,10 @@ public class ProjectEntityCrudKitSettings_SerializationTestCase {
 
     private ProjectEntityCrudKitSettings settings;
 
-    @Before
+    @Autowired
+    private JacksonTester<ProjectEntityCrudKitSettings> tester;
+
+    @BeforeEach
     public void setUp() {
         settings = ProjectEntityCrudKitSettings.get(projectId,
                                                     EntityCrudKitSettings.get(
@@ -35,6 +47,10 @@ public class ProjectEntityCrudKitSettings_SerializationTestCase {
 
     @Test
     public void shouldRoundTrip() throws IOException {
-        
+        var json = tester.write(settings);
+        System.out.println(json.getJson());
+        var parsed = tester.parse(json.getJson());
+        var parsedContent = parsed.getObject();
+        assertThat(parsedContent).isEqualTo(settings);
     }
 }
