6ab7fe4162121ed1280d120ae089e6b43e788d80
Matthew Horridge <matthew.horridge@stanford.edu>
2024-12-02 14:01:38 -0800

    Added constructor that omits the resultsSetFilter for backwards compatibility

--- src/main/java/edu/stanford/protege/webprotege/search/PerformEntitySearchAction.java
+++ src/main/java/edu/stanford/protege/webprotege/search/PerformEntitySearchAction.java
@@ -29,13 +29,14 @@ import static java.util.Objects.requireNonNullElse;
 
 
 @JsonTypeName("webprotege.search.PerformEntitySearch")
-public record PerformEntitySearchAction(@JsonProperty(value = "projectId", required = true) @Nonnull ProjectId projectId,
-                                        @JsonProperty(value = "searchString", required = true) @Nonnull String searchString,
-                                        @JsonProperty(value = "entityTypes", defaultValue = "[]") @Nonnull Set<EntityType<?>> entityTypes,
-                                        @JsonProperty("langTagFilter") @Nonnull LangTagFilter langTagFilter,
-                                        @JsonProperty("searchFilters") @Nonnull ImmutableList<EntitySearchFilter> searchFilters,
-                                        @JsonProperty("pageRequest") @Nonnull PageRequest pageRequest,
-                                        @JsonProperty("resultsSetFilter") @Nullable EntityMatchCriteria resultsSetFilter) implements ProjectAction<PerformEntitySearchResult>, HasProjectId {
+public record PerformEntitySearchAction(
+        @JsonProperty(value = "projectId", required = true) @Nonnull ProjectId projectId,
+        @JsonProperty(value = "searchString", required = true) @Nonnull String searchString,
+        @JsonProperty(value = "entityTypes", defaultValue = "[]") @Nonnull Set<EntityType<?>> entityTypes,
+        @JsonProperty("langTagFilter") @Nonnull LangTagFilter langTagFilter,
+        @JsonProperty("searchFilters") @Nonnull ImmutableList<EntitySearchFilter> searchFilters,
+        @JsonProperty("pageRequest") @Nonnull PageRequest pageRequest,
+        @JsonProperty("resultsSetFilter") @Nullable EntityMatchCriteria resultsSetFilter) implements ProjectAction<PerformEntitySearchResult>, HasProjectId {
 
     public static final String CHANNEL = "webprotege.search.PerformEntitySearch";
 
@@ -53,6 +54,10 @@ public record PerformEntitySearchAction(@JsonProperty(value = "projectId", requi
         this.searchFilters = requireNonNullElse(searchFilters, ImmutableList.of());
         this.pageRequest = requireNonNullElse(pageRequest, PageRequest.requestFirstPage());
         this.resultsSetFilter = requireNonNullElse(resultsSetFilter, EntityTypeIsOneOfCriteria.get(ImmutableSet.copyOf(entityTypes)));
+    }
+
+    public PerformEntitySearchAction(@Nonnull ProjectId projectId, @Nonnull String searchString, @Nonnull Set<EntityType<?>> entityTypes, @Nonnull LangTagFilter langTagFilter, @Nonnull ImmutableList<EntitySearchFilter> searchFilters, @Nonnull PageRequest pageRequest) {
+        this(projectId, searchString, entityTypes, langTagFilter, searchFilters, pageRequest, null);
     }
 
     @Override
